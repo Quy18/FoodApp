5d074ca462debe099d32e093e707c3583be43a6a
DELL <maiquy18092003@gmail.com>
2024-10-31 20:32:39 +0700

    đẩy project

---
+++ app/src/main/java/com/example/foodapp/Activity/BaseActivity.java
@@ -0,0 +1,30 @@
+package com.example.foodapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.foodapp.R;
+import com.example.foodapp.databinding.ActivityIntroBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BaseActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    public String TAG = "uilover";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+    }
+}
